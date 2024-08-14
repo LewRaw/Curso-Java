16017d598c10774796924b77e2babe7b95eee0a4
Luiz Gustavo de Salles Pinho <103963931+LewRaw@users.noreply.github.com>
2024-08-13 21:22:37 -0300

    Voltando a ativa

---
+++ Projetos/jogoXadrez/src/main/java/com/mycompany/jogoxadrez/JogoXadrez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jogoxadrez;
+
+/**
+ *
+ * @author yMode
+ */
+public class JogoXadrez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
